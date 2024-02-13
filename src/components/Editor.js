7134bef054ee54c3c91e47458c6731144dc773d1
Darshan <darshankdarsh01@gmail.com>
2024-02-13 16:06:26 +0530

    Design editor Page

---
+++ src/components/Editor.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Editor = () => {
+  return <div>Editor Goes Here...</div>;
+};
+export default Editor;
--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,3 +1,33 @@
-export default function EditorPage() {
-  return <div>Editor page</div>;
-}
+import React, { useState } from "react";
+import Client from "../components/Client";
+
+import Editor from "../components/Editor";
+const EditorPage = () => {
+  const [clients, setClients] = useState([
+    { socketId: 1, username: "Jeevan Kumar" },
+    { socketId: 2, username: "Darshan K" },
+  ]);
+  return (
+    <div className="main-wrap">
+      <div className="aside">
+        <div className="aside-inner">
+          <div className="logo">
+            <img className="logo-image" src="" alt="logo" />
+          </div>
+          <h3>Connected</h3>
+          <div className="clients-list">
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
+          </div>
+        </div>
+        <button className="btn copy-btn">Copy ROOM ID</button>
+        <button className="btn leave-btn">Leave</button>
+      </div>
+      <div className="editor-wrap">
+        <Editor />
+      </div>
+    </div>
+  );
+};
+export default EditorPage;
