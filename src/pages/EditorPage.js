75c84dbe2edcb689b08115b100dac407e21784ae
Darshan <darshankdarsh01@gmail.com>
2024-02-15 17:13:20 +0530

    WS error

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -3,27 +3,74 @@ import Client from "../components/Client";
 import Editor from "../components/Editor";
 import { initSocket } from "../socket";
 import ACTIONS from "../Actions";
-import { useLocation } from "react-router-dom";
+import toast from "react-hot-toast";
+import {
+  useLocation,
+  useNavigate,
+  Navigate,
+  useParams,
+} from "react-router-dom";
 
 function EditorPage() {
   const socketRef = useRef(null);
   const location = useLocation();
+  const codeRef = useRef(null);
+  const reactNavigator = useNavigate();
+  const { roomId } = useParams();
+  const [clients, setClients] = useState([]);
 
   useEffect(() => {
     const init = async () => {
       socketRef.current = await initSocket();
+      socketRef.current.on("connect_error", (err) => handleErrors(err));
+      socketRef.current.on("connect_failed", (err) => handleErrors(err));
+
+      function handleErrors(e) {
+        console.log("socket error", e);
+        toast.error("Socket connection failed, try again later.");
+        reactNavigator("/");
+      }
       socketRef.current.emit(ACTIONS.JOIN, {
         roomId,
         userName: location.state?.userName,
       });
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({ clients, username, socketId }) => {
+          if (username !== location.state?.username) {
+            toast.success(`${username} joined the room.`);
+            console.log(`${username} joined`);
+          }
+          setClients(clients);
+          socketRef.current.emit(ACTIONS.SYNC_CODE, {
+            code: codeRef.current,
+            socketId,
+          });
+        }
+      );
+      //listen for disconnected
+      socketRef.current.on(ACTIONS.DISCONNECTED, ({ socketId, username }) => {
+        toast.success(`${username} left the room.`);
+        setClients((prev) => {
+          return prev.filter((client) => client.socketId !== socketId);
+        });
+      });
     };
+
     init();
+    return () => {
+      if (socketRef.current !== null) {
+        socketRef.current.disconnect();
+        socketRef.current.off(ACTIONS.JOINED);
+        socketRef.current.off(ACTIONS.DISCONNECTED);
+      }
+    };
   }, []);
 
-  const [clients, setClients] = useState([
-    { socketId: 1, username: "Jeevan Kumar" },
-    { socketId: 2, username: "Darshan K" },
-  ]);
+  if (!location.state) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <div className="main-wrap">
       <div className="aside">
